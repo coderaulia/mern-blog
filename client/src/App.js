4f67de71fd302e6dc300b5d3a8fed0535139f57e
coderaulia <auliasw89@gmail.com>
2021-09-30 04:24:59 +0700

    testing react router

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,27 @@ import Write from "./Pages/write/Write";
 import Setting from "./Pages/setting/Setting";
 import Login from "./Pages/login/Login";
 import Register from "./Pages/register/Register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+	// user testing
+	const user = false;
 	return (
-		<>
+		<Router>
 			<TopBar />
-			<Register />
-		</>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/register">{user ? <Home /> : <Register />}</Route>
+				<Route path="/login">{user ? <Home /> : <Login />}</Route>
+				<Route path="/write">{user ? <Write /> : <Login />}</Route>
+				<Route path="/settings">{user ? <Setting /> : <Login />}</Route>
+				<Route path="/post/:postId">
+					<SinglePost />
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
--- client/src/parts/topbar/TopBar.jsx
+++ client/src/parts/topbar/TopBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 import './topbar.css'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
@@ -6,6 +7,8 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function TopBar() {
+   // user session testing
+   const user = true;
    return (
       <div className="top">
          <div className="topLeft">
@@ -15,15 +18,38 @@ export default function TopBar() {
          </div>
          <div className="topCenter">
             <ul className="topMenu">
-               <li className="topMenuItem">Home</li>
-               <li className="topMenuItem">About</li>
-               <li className="topMenuItem">Contact</li>
-               <li className="topMenuItem">Write now!</li>
-               <li className="topMenuItem">Logout</li>
+               <li className="topMenuItem">
+                  <Link className="link" to="/">Home</Link>
+               </li>
+               <li className="topMenuItem"><Link className="link" to="/">About</Link></li>
+               <li className="topMenuItem"><Link className="link" to="/">Contact</Link></li>
+               <li className="topMenuItem"><Link className="link" to="/">Write</Link></li>
+               <li className="topMenuItem"><Link className="link" to="/">Logout</Link></li>
             </ul>
          </div>
          <div className="topRight">
-            <img className="topImg" src="https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt=""/>
+            {user ? (
+            <Link className="link" to="/settings">
+               <img
+               className="topImg"
+               src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+               alt=""
+               />
+            </Link>
+             ) : (
+            <ul className="topList">
+               <li className="topListItem">
+               <Link className="link" to="/login">
+                  Login
+               </Link>
+               </li>
+               <li className="topListItem">
+               <Link className="link" to="/register">
+                  Register
+               </Link>
+               </li>
+            </ul>
+         )}
             <SearchIcon className="searchIcon"/>
          </div>
 
